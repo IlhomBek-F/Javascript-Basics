6af4d5a09079eac0c0a4c2ac578de28b42079040
Ilhombek-F <futurewithme1221@gmail.com>
2021-04-12 21:36:00 +0400

    Js
    data-types and variables

---
+++ data-types-and-variables/script.js
@@ -0,0 +1,19 @@
+/* Data Types:
+   undifined, null , boolean, string, symbol, number , and object
+  
+   undifined ---> is not undifined
+   null --> is nothing
+   boolean --> true or false
+   string ---> is any sort of text
+   number --> is number
+   symbol --> is uniqie
+   object --> able store a lot of key values
+*/
+
+var myName = "Ilhomjon";
+
+myName = 8;
+
+let ourName = "CodingCamp";
+
+const pi = 3.14;
---
+++ string-number/script.js
@@ -0,0 +1,19 @@
+let string = "This is the normal string"; // string
+let number = 42; // number
+let bool = false; // boolian (false or true)
+let array = [1, 2, 3, 4, "a", "b", "c"];
+let Undefined = undefined;
+let Null = null;
+let nan = NaN;
+
+let object = {
+  name: "Ilhomjon",
+};
+
+console.log(object.name);
+console.log(array[3]);
+//string
+//number
+//bool
+//array[3] -> 4
+// object.
